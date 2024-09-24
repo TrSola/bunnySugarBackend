e3bf5811c5c90746e9423392f32ea8cd69cbd950
JWen33 <jww1033@gmail.com>
2024-09-24 14:35:12 +0800

    feat OpenAPI
    可以使用swagger-ui去測試API，localhost:8080/apidoc
    
    2024/09/24 14:34

---
+++ src/main/java/com/EEIT85/bunnySugar/config/OpenAPIDocConfig.java
@@ -0,0 +1,30 @@
+package com.EEIT85.bunnySugar.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition()
+public class OpenAPIDocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI() {
+        OpenAPI openApi = new OpenAPI();
+        Components components = new Components();
+        Info info = new Info();
+        info.title("BunnySugar API 文件").version("V.2024092401").description("test");
+
+
+        openApi
+                .info(info)
+                .components(components);
+
+
+
+        return openApi;
+    }
+}
