2ee8a7842a0322cffc5726768947242ee6374b2b
Hightwo1028 <istoo102800@gmail.com>
2024-09-25 16:42:54 +0800

    modify allowed path in WebConfig
    修正允許的路徑
    
    2024/09/25 16:42

--- src/main/java/com/EEIT85/bunnySugar/config/WebConfig.java
+++ src/main/java/com/EEIT85/bunnySugar/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 設定應用於所有路徑
-                .allowedOrigins("http://localhost:5173") // 允許的前端 URL，根據實際情況修改
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174") // 允許的前端 URL，根據實際情況修改
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允許的方法
                 .allowedHeaders("*") // 允許的頭部信息
                 .allowCredentials(true); // 是否允許憑證
