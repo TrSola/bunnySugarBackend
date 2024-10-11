94a3fdc1c325bb5e8cf4099397ac3d9f2bda600e
Hightwo1028 <istoo102800@gmail.com>
2024-10-11 14:17:29 +0800

    新增視窗 在找方法應用
    new a JFrame window
    
    2024/10/11 14:17

--- src/main/java/com/EEIT85/bunnySugar/controller/ChatWebSocketController.java
+++ src/main/java/com/EEIT85/bunnySugar/controller/ChatWebSocketController.java
@@ -1,6 +1,8 @@
 package com.EEIT85.bunnySugar.controller;
 
 import com.EEIT85.bunnySugar.dto.ChatMessage; // 引用 DTO
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,6 +16,7 @@ import java.util.Map;
 @CrossOrigin
 public class ChatWebSocketController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ChatWebSocketController.class);
     private final SimpMessagingTemplate messagingTemplate;
 
     // 儲存用戶的 WebSocket 會話
@@ -25,9 +28,12 @@ public class ChatWebSocketController {
 
     @MessageMapping("/send") // 當接收到 /app/send 的訊息時
     public void sendMessage(@Payload ChatMessage chatMessage) {
+        // 打印接收到的訊息到控制台
+        logger.info("Received message: " + chatMessage.getContent() + " from recipient: " + chatMessage.getRecipientId());
+
         // 根據用戶ID發送訊息
         String recipientId = chatMessage.getRecipientId(); // 從訊息中獲取接收者ID
-        messagingTemplate.convertAndSendToUser(recipientId, "/topic/messages", chatMessage.getContent());
+        messagingTemplate.convertAndSendToUser(recipientId, "/topic/messages", chatMessage); // 傳送整個 ChatMessage 物件
     }
 
     // 用於管理用戶會話
