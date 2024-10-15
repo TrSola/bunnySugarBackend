23ed9d50d9eac1f5d371b501b1ebf2a7009a7d6b
Hightwo1028 <istoo102800@gmail.com>
2024-10-15 23:44:48 +0800

    ws service 修修改
    modify ws service
    
    2024/10/15 23:44

--- src/main/java/com/EEIT85/bunnySugar/repository/ChatMessageRepository.java
+++ src/main/java/com/EEIT85/bunnySugar/repository/ChatMessageRepository.java
@@ -10,5 +10,4 @@ import java.util.List;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
     // 查詢用戶與管理員之間的聊天記錄
     List<ChatMessage> findBySenderIdAndRecipientId(String senderId, String recipientId);
-    List<ChatMessage> findByRecipientIdAndSenderId(String recipientId, String senderId);
 }
