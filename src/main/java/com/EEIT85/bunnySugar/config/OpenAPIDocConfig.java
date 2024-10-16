ec8d28a9220f5b8acf42c418cc2b9160db9144c3
JWen33 <jww1033@gmail.com>
2024-10-16 11:06:41 +0800

    fix 修正swaggerUI JWT認證問題
    設定可以由使用者於前端設定JWT token
    
    2024/10/16 11:06

--- src/main/java/com/EEIT85/bunnySugar/config/OpenAPIDocConfig.java
+++ src/main/java/com/EEIT85/bunnySugar/config/OpenAPIDocConfig.java
@@ -1,6 +1,9 @@
 package com.EEIT85.bunnySugar.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,7 +11,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition()
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "My API",
+                version = "1.0",
+                description = "This is a sample API with Bearer Token authentication",
+                termsOfService = "https://example.com/terms",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "API Support",
+                        email = "support@example.com",
+                        url = "https://example.com/support"
+                ),
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth") // 使用 bearer token 的安全需求
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT" // 声明使用 JWT 格式
+)
 public class OpenAPIDocConfig {
 
     @Bean
