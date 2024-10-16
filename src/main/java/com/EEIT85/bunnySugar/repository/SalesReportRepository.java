19e6dfad9fd646698f2d0ed4490aacd161a0a053
JWen33 <jww1033@gmail.com>
2024-10-16 23:16:05 +0800

    feat 修改銷售數據邏輯
    透過營業額排序商品
    
    2024/10/16 11:16

--- src/main/java/com/EEIT85/bunnySugar/repository/SalesReportRepository.java
+++ src/main/java/com/EEIT85/bunnySugar/repository/SalesReportRepository.java
@@ -16,7 +16,8 @@ public interface SalesReportRepository extends JpaRepository<Orders, Long> {
     @Query("SELECT new com.EEIT85.bunnySugar.dto.orders.admin.ProductSalesDto(p.id, p.productName, od.price, SUM(od.price * od.quantity), SUM(od.quantity)) " +
             "FROM OrderDetails od " +
             "JOIN od.products p " +
-            "GROUP BY p.id, p.productName, od.price")
+            "GROUP BY p.id, p.productName, od.price "+
+            "ORDER BY SUM(od.price * od.quantity) DESC")
     List<ProductSalesDto> findSalesReport();
 
     // 查詢特定區間的產品銷售數據（包括單價、總銷售額和數量）
@@ -25,7 +26,8 @@ public interface SalesReportRepository extends JpaRepository<Orders, Long> {
             "JOIN od.products p " +
             "JOIN od.orders o " +
             "WHERE o.createTime BETWEEN :startDateTime AND :endDateTime " +
-            "GROUP BY p.id, p.productName, od.price")
+            "GROUP BY p.id, p.productName, od.price "+
+            "ORDER BY SUM(od.price * od.quantity) DESC")
     List<ProductSalesDto> findSalesReportByTimeRange(@Param("startDateTime") LocalDateTime startDateTime,
                                                      @Param("endDateTime") LocalDateTime endDateTime);
 
