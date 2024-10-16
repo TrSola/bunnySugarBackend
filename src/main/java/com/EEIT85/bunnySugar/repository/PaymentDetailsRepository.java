3cdb6b8aa1acd8f4142d0aacc82b6dfef04b7505
Willy <willy718066@gmail.com>
2024-10-16 11:00:41 +0800

    feat 修改訂單抓取邏輯
    加上抓取時間範圍及付款方式
    
    2024/10/16 11:00

--- src/main/java/com/EEIT85/bunnySugar/repository/PaymentDetailsRepository.java
+++ src/main/java/com/EEIT85/bunnySugar/repository/PaymentDetailsRepository.java
@@ -19,10 +19,20 @@ public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, 
     int updatePaymentStatus(@Param("orderId") Long orderId, @Param("paymentStatus") String paymentStatus);
 
 
-    @Query("SELECT pd.merchantNo FROM PaymentDetails pd " +
-            "JOIN pd.orders o " +
-            "JOIN o.user u " +
-            "WHERE u.id = :userId")
+//    @Query("SELECT pd.merchantNo FROM PaymentDetails pd " +
+//            "JOIN pd.orders o " +
+//            "JOIN o.user u " +
+//            "WHERE u.id = :userId AND pd.paymentStatus = '未付款'")
+//    List<String> findMerchantNoByUserId(@Param("userId") Long userId);
+
+    @Query(value = "SELECT pd.merchant_no " +
+            "FROM payment_details pd " +
+            "INNER JOIN Orders o ON pd.orders_id = o.id " +
+            "INNER JOIN Users u ON o.users_id = u.id " +
+            "WHERE u.id = :userId " +
+            "AND pd.payment_status = '未付款' " +
+            "AND DATE(pd.create_time) >= CURDATE() AND payment_method = '信用卡付款'",
+            nativeQuery = true)
     List<String> findMerchantNoByUserId(@Param("userId") Long userId);
 
     @Query("SELECT pd FROM PaymentDetails pd WHERE pd.merchantNo = :merchantNo")
